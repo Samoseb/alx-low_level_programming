5d1b4f11798484776c83dd10f0ed2f860aed65c8
Samoseb <osebsam03@gmail.com>
2022-03-14 14:24:01 -0700

    Using puts std output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
- * * main - print the string in the put function
- * *
- * * Description: using the main function
- * * this program prints "Programming is like building a multilingual puzzle
- * * Return: 0
- * */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+ *  This program is functioning through using putc standard outputs
+ *  to print the word 
+ *  "Programming is like building a multilingual puzzle
+ *  /
+  int main(void)
+  {
+  puts("\"Programming is like building multilingualnpuzzle");
+  return (0);
+  }
